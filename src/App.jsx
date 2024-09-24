b3cb1eeeba3b6080c9b3315bf68d8f13447a6e52
Yogesh Limbani <yrlimbani03@gmail.com>
2024-09-24 17:37:32 +0530

    Implement environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ const App = () => {
   const [data, setData] = useState(null);
 
   return (
-        <>
-        <h1 className='text-4xl w-full text-center p-2 border-b-2 border-solid border-black'>Enhancing Text Insights Through API-Driven Natural Language Processing</h1>
-        <div className='flex flex-row justify-between m-4'>
+    <>
+      <h1 className='text-4xl w-full text-center p-2 border-b-2 border-solid border-black'>Enhancing Text Insights Through API-Driven Natural Language Processing</h1>
+      <div className='flex flex-row justify-between m-4'>
         <div className='flex flex-col justify-start items-start m-6 gap-y-4'>
-        <TextBox data={data} setData={setData} />
-        <DisplayResult data={data} />
+          <TextBox setData={setData} />
+          <DisplayResult data={data} />
         </div>
-        {data && (<BarChart displayData={data}/>)}
-        </div>
-        </>
+        {data && (<BarChart displayData={data} />)}
+      </div>
+    </>
   );
 };
 
--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,27 +1,33 @@
 import { useState } from 'react'
 import axios from 'axios';
 
-function TextBox({data, setData}) {
-    const [inputText, setInputText] = useState('');
-    const analyzeText = async () => {
-        try {
-          const response = await axios.post(
-            'http://localhost:5000/api/textrazor',
-            { text: inputText }
-          );
-          setData(response.data);
-          console.log('Analysis results:', response.data);
-        } catch (error) {
-          console.error('Error analyzing text:', error);
+function TextBox({ setData }) {
+  const [inputText, setInputText] = useState('');
+  const analyzeText = async () => {
+    try {
+      const response = await axios.post(
+        'https://cors-anywhere.herokuapp.com/https://api.textrazor.com/',
+        `text=${encodeURIComponent(inputText)}&extractors=entities`,
+        {
+          headers: {
+            'x-textrazor-key': import.meta.env.VITE_RAZOR_KEY,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
         }
-      };
+      );
+      setData(response.data.response);
+      console.log('Analysis results:', response.data.response);
+    } catch (error) {
+      console.error('Error analyzing text:', error);
+    }
+  };
   return (
     <>
-    <h2 className='bold text-4xl p-2'>Text Analysis</h2>
-    <div className="flex flex-col justify-start items-center">
-    <textarea id="message" rows="5" cols="56" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-4" placeholder="Enter text here..." value={inputText} onChange={(e) => setInputText(e.target.value)}></textarea>
-    <button onClick={analyzeText} className="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true" type="submit"> Analyze </button>
-    </div>
+      <h2 className='bold text-4xl p-2'>Text Analysis</h2>
+      <div className="flex flex-col justify-start items-center">
+        <textarea id="message" rows="5" cols="56" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-4" placeholder="Enter text here..." value={inputText} onChange={(e) => setInputText(e.target.value)}></textarea>
+        <button onClick={analyzeText} className="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true" type="submit"> Analyze </button>
+      </div>
     </>
   )
 }
